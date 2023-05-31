a1fa644da3583a254a5dbade775403b7a8cb104b
Dixie Dean <d.w.sokolov26@gmail.com>
2023-05-31 15:13:31 +0300

    List of threads added

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         CommonResource commonResource = new CommonResource();
         Route route = new Route(commonResource);
+        List<Thread> threads = new ArrayList<>();
 
         for (int i = 0; i < 1000; i++) {
             Thread thread = new Thread(route);
             thread.setName("Thread: " + i);
+            threads.add(thread);
             thread.start();
         }
 
-        Thread.sleep(100);
+        for (Thread thread : threads) {
+            thread.join();
+        }
 
         commonResource.submitResults();
     }
